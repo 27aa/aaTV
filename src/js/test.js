d51252b8833654b68f68811cb39c097ed6237914
Adil Ahmed <adil.ahmd@eduge.ch>
2026-09-02 15:40:28 +0200

    recup du group reussi dans test.js

--- src/js/test.js
+++ src/js/test.js
@@ -1,7 +1,8 @@
-const m3u = '#EXTINF: group-title="Albania",Kanali 7 Ⓢ https://fe.tring.al/delta/105/out/u/1200_1.m3u8';
+const m3u = '#EXTINF:-1 tvg-name="Qatar Television" tvg-logo="https://i.imgur.com/N5RB4sp.png" tvg-id="QatarTelevision.qa" tvg-chno="1" tvg-country="QA" group-title="Qatar",Qatar Television';
+const groupLength = 'group-title="'.length;
 // const cleanM3u = m3u.trim();
 
 const n1 = m3u.indexOf('group-title="');
-const n2 = m3u.indexOf('"', n1 + 'group-title="'.length);
-const group = m3u.slice(n1 + 'group-title="'.length, n2 - (n1 + 'group-title="'.length));
-alert(`n1 : ${n1} \nn2 : ${n2} \ngroup-title : ${'group-title="'.length}  \ngroup : ${group}`);
+const n2 = m3u.indexOf('"', n1 + groupLength);
+const group = m3u.slice(n1 + groupLength, n2);
+alert(`n1 : ${n1} \nn2 : ${n2} \ngroup-title : ${groupLength}  \ngroup : ${group}`);
